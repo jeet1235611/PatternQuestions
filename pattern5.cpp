bca4c061d5ac09489d1c7ec6344300a1f9dd96aa
acoustic_jeet <js.jitendra11@gmail.com>
2018-04-24 11:26:50 +0530

    Create pattern5.cpp

---
+++ pattern5.cpp
@@ -0,0 +1,30 @@
+5
+*       *
+**     **
+***   ***
+**** ****
+**********
+
+#include <iostream>
+using namespace std;
+int main(){
+        int n;
+        cout << "Enter the value of n\n";
+        cin >> n;
+        int stars = 1, spaces = 2*n-3;
+        for(int row = 1; row <= n; row++){
+                //for left stars
+                for(int i = 1; i <= stars; i++)
+                        cout <<"*";
+                //for center spaces
+                for(int i = 1; i <= spaces; i++)
+                        cout <<" ";
+                //for right stars
+                for(int i = 1; i <= stars; i++)
+                        cout <<"*";
+                cout << endl;
+                spaces = spaces - 2;
+                stars = stars + 1;
+
+        }
+}
