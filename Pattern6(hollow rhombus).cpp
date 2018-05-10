1a8c9c75a108151dcc912da70e0ca355ddcbbf37
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-10 19:08:00 +0530

    Create Pattern6(hollow rhombus).cpp

---
+++ Pattern6(hollow rhombus).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int n;
+        cout << "Enter the value of n\n";
+        cin >> n;
+
+        int i = n/2,j = n/2;
+        for(int row = 0; row < n; row++){
+                for(int star = 0; star <= j; star++){
+                if(star == i || star == j)
+                        cout << "*";
+                else
+                        cout << " ";
+                }
+
+                if(row < n/2){
+                        i--;
+                        j++;
+                }
+
+                else {
+                        i++;
+                        j--;
+                }
+
+                cout << endl;
+        }
+}
+
+
+
+Enter the value of n
+9
+    *
+   * *
+  *   *
+ *     *
+*       *
+ *     *
+  *   *
+   * *
+    *
